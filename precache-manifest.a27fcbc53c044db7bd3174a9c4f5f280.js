d3bc89e3e38f8e8d3f26af7dc6eb4db016f916f7
vss-s <53346463+vss-s@users.noreply.github.com>
2020-04-27 11:12:57 +0300

    Updates

---
+++ precache-manifest.a27fcbc53c044db7bd3174a9c4f5f280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a39e02db499bd14f178fd4229d47a8a",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "c43a460d5090470cba9a",
+    "url": "/goit-react-hw-02-feedback/static/css/main.00acc6fb.chunk.css"
+  },
+  {
+    "revision": "9b75abb5003c03c19f01",
+    "url": "/goit-react-hw-02-feedback/static/js/2.892cb972.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.892cb972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43a460d5090470cba9a",
+    "url": "/goit-react-hw-02-feedback/static/js/main.8d09f526.chunk.js"
+  },
+  {
+    "revision": "921f1dce578c37a88de9",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.912650e5.js"
+  }
+]);
